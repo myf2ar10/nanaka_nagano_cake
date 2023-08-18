b45797dfad38a43148b866fc7afcd197a474e296
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-18 08:18:08 +0000

    admin/itemsの編集、items_controller、itemsのビューファイル

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,38 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+    @genre = Genre.all
+  end
+  def create
+    item = Item.new(item_params)
+    item.save
+    flash[:notice] = "新規登録に成功しました。"
+    redirect_to admin_item_path(item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+  
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    flash[:notice] = "編集に成功しました。"
+    redirect_to admin_item_path(item.id)
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :detail, :price, :is_active, :image)
+  end
+  
 end
