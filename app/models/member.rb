d2f45e42736471f5c95fa84e00a8579ff0b9f2df
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-08-18 12:03:52 +0000

    [add]ジャンル登録

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,4 +7,13 @@ class Member < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
+  
+  validates :family_name,       presence: true
+  validates :family_name_kana,  presence: true
+  validates :first_name,        presence: true
+  validates :first_name_kana,   presence: true
+  validates :email,             presence: true, uniqueness: true
+  validates :post_code,         presence: true
+  validates :address,           presence: true
+  validates :telephone_number,  presence: true
 end
