8e4159a847fec5bb2b02eb74d953018b24d09151
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-19 12:07:20 +0000

    修正の保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admin_orders_path
-    when Member
-      members_mypage_path
-    end
-  end
 
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :is_deleted])
-  end
 
 end
