1e2220ebc42a60c0c6de55f6d3f76c2829f54fc6
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-08-19 11:20:19 +0000

    admin/member

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -11,15 +11,17 @@ class Admin::MembersController < ApplicationController
   def update
     @member = Member.find(params[:id])
     if @member.update(member_params)
+      flash[:notice] = "会員情報を変更しました"
       redirect_to admin_member_path(@member.id)
     else
       @member = Member.find(params[:id])
+      flash[:notice] = "会員情報の変更に失敗しました"
       render :edit
     end
   end
 
   def index
-    @members = Member.all
+    @members = Member.page(params[:page])
   end
 
   private
