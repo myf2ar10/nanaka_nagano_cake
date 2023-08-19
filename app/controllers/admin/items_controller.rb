6c557863ae2552e14c4ed240483e0f5a50944fae
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-19 08:12:42 +0000

    item、show、indexの編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,18 +21,18 @@ class Admin::ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     item = Item.find(params[:id])
     item.update(item_params)
     flash[:notice] = "編集に成功しました。"
     redirect_to admin_item_path(item.id)
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id, :name, :detail, :price, :is_active, :image)
   end
-  
+
 end
