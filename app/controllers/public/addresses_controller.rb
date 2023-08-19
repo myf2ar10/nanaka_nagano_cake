84393b0eb0c31f2bbd104466e4d07cd64485d7b9
Wataru Nakamura <130727655+wataengen@users.noreply.github.com>
2023-08-19 19:44:28 +0900

    Update addresses_controller.rb
    条件分岐を追加、リダイレクト先をaddresses_pathに変更しました

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,14 +1,20 @@
 class Public::AddressesController < ApplicationController
   def index
-    @addresses = Address.all
+    @addresses = Address.where(member_id: current_member.id)
     @address = Address.new
   end
   
   def create
-    address = Address.new(address_params)
-    address.save
-    flash[:notice] = "住所を新規登録しました。"
-    redirect_to address_path
+      address = Address.new(address_params)
+    if address.save
+      flash[:notice] = "住所を新規登録しました。"
+      redirect_to address_path
+    else
+      @addresses = Address.where(member_id: current_member.id)
+      @address = Address.new
+      flash[:notice] = "住所の新規登録に失敗しました。"
+      render index
+    end
   end
 
   def edit
@@ -16,17 +22,28 @@ class Public::AddressesController < ApplicationController
   end
   
   def update
-    address = Address.find(params[:id])
-    address.update(address_params)
-    flash[:notice] = "住所の編集が完了しました。"
-    redirect_to address_path(address.id)
+       address = Address.find(params[:id])
+    if address.update(address_params)
+       flash[:notice] = "住所の編集が完了しました。"
+       redirect_to addresses_path
+    else
+      @address = Address.find(params[:id])
+      flash[:notice] = "住所の編集に失敗しました。"
+      render edit
+    end
   end
   
   def destroy
-    address = Address.find(params[:id])
-    address.destroy
-    flash[:notice] = "住所を削除しました。"
-    redirect_to address_path
+       address = Address.find(params[:id])
+    if address.destroy
+       flash[:notice] = "住所を削除しました。"
+       redirect_to addresses_path
+    else
+       flash[:notice] = "住所を削除に失敗しました。"
+       @addresses = Address.where(member_id: current_member.id)
+       @address = Address.new
+       render index
+    end
   end
   
   private
