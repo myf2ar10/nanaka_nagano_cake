c52c31ef064bb3134ab034868bbf060342df5c86
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-08-20 11:32:35 +0000

    [追加]カート機能／[修正]会員側商品一覧

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,5 @@
 class CartItem < ApplicationRecord
-　# カート内の同一商品を追加する時、１０を超えることができてしまうのを阻止するための記述
+# カート内の同一商品を追加する時１０を超えることができてしまうのを阻止するための記述
   # validates :quantity, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 
   belongs_to :member
