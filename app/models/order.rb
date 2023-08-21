2d85f3c93dd5eb1ad0511bcad89c2126258656bf
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-21 03:45:04 +0000

    developをプル

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
   has_many   :order_details, dependent: :destroy
 
   enum pay_method: { credit_card: 0, transfer: 1 }
-  enum order_status: { waiting: 0, confirm: 1, making: 2, preparing: 3, shipped: 4 }
+  enum status: { waiting: 0, confirm: 1, making: 2, preparing: 3, shipped: 4 }
 end
