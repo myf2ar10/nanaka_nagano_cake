7bd9e7bcaacfb6fb0c00e7a0f3fa779c34b064e5
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-21 07:06:44 +0000

    ステータス表示修正

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,7 +2,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum making_status: { not_available: 0, waiting_for_makiing: 1, making: 2, made: 3 }
+  enum making_status: { not_available: 0, waiting_for_making: 1, making: 2, made: 3 }
 
   def add_tax_price
     (self.price * 1.08).round
