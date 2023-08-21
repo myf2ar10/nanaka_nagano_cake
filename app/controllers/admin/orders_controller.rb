e0ac857d865526597fc22e60cab2a473666f92bc
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-21 03:23:22 +0000

    注文詳細画面、注文ステータス更新機能の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.all
+  end
+  def update
+    order = Order.find(params[:id])
+    if order.save
+      flash[:notice] = "注文ステータスの更新に成功しました。"
+      redirect_to admin_order_path(order.id)
+    else
+      @order = Order.find(params[:id])
+      @order_details = OrderDetail.all
+      flash[:notice] = "注文ステータスの更新に失敗しました。"
+      render show
+    end
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,10 @@
 class OrderDetail < ApplicationRecord
   has_many :order
   has_many :item
-  
+
   enum making_status: { not_available: 0, waiting_for_makiing: 1, making: 2, made: 3 }
+
+  def add_tax_price
+    (self.price * 1.08).round
+  end
 end
