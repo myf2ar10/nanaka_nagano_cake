05fb99b6474dc1a46dedbf8e96355dc98ebc0aa3
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-08-21 16:00:14 +0000

    admin/order_details

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,42 @@
 class Admin::OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: @order.id)
   end
+
   def update
     order = Order.find(params[:id])
+    order.status = params[:order][:status]
+
     if order.update(order_params)
-      flash[:notice] = "注文ステータスの更新に成功しました。"
-      redirect_to admin_order_path(order.id)
+      if order.status == "confirm"
+        order_details = OrderDetail.where(order_id: order.id)
+        order_details.each do |order_detail|
+          order_detail.update(making_status: "waiting_for_making")
+        end
+      elsif order.status == "waiting"
+        order_details = OrderDetail.where(order_id: order.id)
+        order_details.each do |order_detail|
+          order_detail.update(making_status: "not_available")
+        end
+      end
+      redirect_back(fallback_location: root_path)
     else
       @order = Order.find(params[:id])
-      @order_details = OrderDetail.where(order_id: @order.id)
-      flash[:notice] = "注文ステータスの更新に失敗しました。"
-      render "orders/show"
+      @order_details = OrderDetail.where(order_id: order.id)
+      render :show
     end
+
+    # if order.update(order_params)
+    #   flash[:notice] = "注文ステータスの更新に成功しました。"
+    #   redirect_to admin_order_path(order.id)
+    # else
+    #   @order = Order.find(params[:id])
+    #   @order_details = OrderDetail.where(order_id: @order.id)
+    #   flash[:notice] = "注文ステータスの更新に失敗しました。"
+    #   render "orders/show"
+    # end
   end
 
   private
