357ebf3c49befe3d2ffe158b4b23c59763de93b0
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-08-22 11:49:12 +0000

    注文履歴詳細の修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -28,12 +28,14 @@ class Public::OrdersController < ApplicationController
         selected = Address.find(params[:order][:registered_address_id])
         @selected_address = selected.post_code + " " + selected.address + " " + selected.name
       else
+        flash[:notice] = "お届け先を選択してください"
         render :new
       end
     when "new_address"
       unless params[:order][:new_post_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
         @selected_address = params[:order][:new_post_code] + " " + params[:order][:new_address] + " " + params[:order][:new_name]
       else
+        flash[:notice] = "お届け先を記入してください"
         render :new
       end
     end
