f40f2a05e023c3b4c073d1f7a08ccf8602474f0e
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-08-22 09:36:49 +0000

    製作ステータスの修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -79,13 +79,13 @@ class Public::OrdersController < ApplicationController
     end
 
     if @order.save
-      if @order.status == 0
+      if @order.status == 1
         @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, quantity: cart_item.quantity, making_status: 1)
+          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, quantity: cart_item.quantity, making_status: 0)
         end
       else
         @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, quantity: cart_item.quantity, making_status: 0)
+          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, quantity: cart_item.quantity, making_status: 1)
         end
       end
       @cart_items.destroy_all
