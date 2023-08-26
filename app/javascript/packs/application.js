7cdee754a4c59514059a5ba259ef79850d6db7c7
Marin Kato <133328222+asasuzu@users.noreply.github.com>
2023-08-26 16:04:56 +0900

    変な改行の修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -50,4 +50,3 @@ import '../stylesheets/admin/members/show.css';
 import '../stylesheets/admin/orders/individual.css';
 import '../stylesheets/admin/orders/show.css';
 import '../stylesheets/admin/sessions/new.css';
-
